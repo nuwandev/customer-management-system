e78f3c794bd179e201ba46aab113294401bd48ec
Theekshana Nuwan <theekshanabro40@gmail.com>
2025-12-17 21:05:47 +0530

    Add CustomerRepository interface for DB operations

---
+++ backend/customer-management-system/src/main/java/com/nuwandev/cms/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.nuwandev.cms.repository;
+
+import com.nuwandev.cms.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+    
+}
