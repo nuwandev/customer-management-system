a86d0e514d3d31e457ca1fb445d6b73c4c80a397
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-01-17 21:38:33 +0530

    feat(config): update CORS configuration to allow access from production URL

--- backend/customer-management-system-api/src/main/java/com/nuwandev/cms/config/WebConfig.java
+++ backend/customer-management-system-api/src/main/java/com/nuwandev/cms/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://customer-management-system.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
